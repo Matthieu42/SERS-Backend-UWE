e13fcb48da98a12193671d42321105bcf354a344
Matthieu Tinnes <matthieu.tinnes@gmail.com>
2019-07-17 17:08:01 +0100

    allow CORS

---
+++ app/src/Action/User/GetUserAction.php
@@ -0,0 +1,34 @@
+<?php
+namespace App\Action\User;
+
+use App\Service\UserService;
+use Psr\Log\LoggerInterface;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+final class GetUserAction
+{
+    /**
+     * @var UserService
+     */
+    private $userService;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    public function __construct(UserService $userService, LoggerInterface $logger)
+    {
+        $this->userService = $userService;
+        $this->logger = $logger;
+    }
+
+    public function __invoke(Request $request, Response $response, $args)
+    {        
+        $user = $this->userService->getUserById($args['id']);
+        $this->logger->info('User retrieved '.$args['id'] );
+        $response = $response->withJson($user);
+        return $response;
+    }
+}
--- index.php
+++ index.php
@@ -6,4 +6,11 @@ $settings = require __DIR__ . '/app/settings.php';
 $app = new \Slim\App($settings);
 require __DIR__ . '/app/dependencies.php';
 require __DIR__ . '/app/routes.php';
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
 $app->run();
